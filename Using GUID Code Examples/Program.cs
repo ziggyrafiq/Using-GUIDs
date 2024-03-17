b69ffe3edf202f359e8dac6683562d124c7f90fd
Ziggy Rafiq <ziggyrafiq@hotmail.com>
2024-03-17 15:40:25 +0000

    Using GUIDs
    Using GUIDs

---
+++ Using GUID Code Examples/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+Guid testGuid;
+testGuid = Guid.NewGuid();
+
+string guidString = "b86f2096-237a-4059-8329-1bbcea72769b";
+
+//convert string to Guid
+testGuid = Guid.Parse(guidString);
+
+Console.WriteLine(testGuid);
+
+//Best way to convert string to Guid
+if (Guid.TryParse(guidString, out testGuid))
+{
+    Console.WriteLine(testGuid);
+}
+else
+{
+    Console.WriteLine("Sorry Invalid GUID");
+}
+
+//Comparing two Guids 
+Guid guid1 = Guid.NewGuid();
+Guid guid2 = Guid.NewGuid();
+if (guid1.Equals(guid2))
+{
+    Console.WriteLine("GUID Are Equal.");
+}
+else
+{
+    Console.WriteLine("Sorry GUID Not Equal.");
+}
+
+//Guid to string
+string testGuidString = testGuid.ToString();
+Console.WriteLine(testGuidString);
+
+
+Console.ReadLine();
